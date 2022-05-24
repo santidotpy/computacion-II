42062c30f4c3ac8ba6f98e2a31f9f6132782a346
santidotpy <santidotpy@gmail.com>
2022-05-23 22:17:34 -0300

    ✨ matrix recargado con pool

--- practicas/neo_matrix.py
+++ practicas/neo_matrix.py
@@ -1,47 +1,51 @@
 import argparse
+from math import log10
 import numpy as np
 from multiprocessing import Pool
+
 
 def read_matrix(txt):
     with open(txt, 'r') as f:
         l = [[int(num) for num in line.split(',')] for line in f]
     return l
 
+def calcuate_pot(matriz):
+    for item in matriz:
+        print(item ** item)
 
-def calcuate(matriz, operation):
-    if operation == 'raiz':
-        pass
-    elif operation == 'pot':
-        for i in range(len(matrix)):
-            print(matrix[0][i] ** matrix[0][i])
-    elif operation == 'log':
-        pass
-    else:
-        raise ValueError
-
-
-
+def calculate_sqrt(matriz):
+    for item in matriz:
+        print(item ** 0.5)
+        
+def calculate_log(matriz):
+    for item in matriz:
+        print(np.log10(item))
 
 
 
 if __name__ == '__main__':
     parser = argparse.ArgumentParser()
-    #parser.add_argument("-c", "--calculo", help="calculo a utilizar")
+    parser.add_argument("-c", "--calculo", help="calculo a utilizar")
     parser.add_argument("-f", "--file", help="archivo donde se encuentra la matriz")
     parser.add_argument("-p", "--processes", default=2, help="cantidad de procesos")
 
     args = parser.parse_args()
     txt = args.file
-    p = args.processes
+    p = int(args.processes)
+    c = args.calculo
     pool = Pool(processes=p)
 
     matrix = read_matrix(txt)
-    #matrix = np.array(matrix)
+    matrix = np.array(matrix)
+    matrix = np.split(matrix, len(matrix))
 
-    for i in range(len(matrix)):
-        print(matrix[0][i] * 5)
-        #for j in range(len(matrix)):
-        #    matrix[i][j] = matrix[i][j] ** matrix[i][j]
 
-    #print(len(matrix))
-    calcuate(matrix, 'pot')
+    if c == 'pot':
+        pool.map(calcuate_pot, matrix)
+    elif c == 'raiz':
+        pool.map(calculate_sqrt, matrix)
+    elif c == 'log':
+        pool.map(calculate_log, matrix)
+
+    pool.close()
+    pool.join()

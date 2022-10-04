a52e838bca3bfc08e242ec6890787d91b6608972
santidotpy <santidotpy@gmail.com>
2022-10-03 23:44:00 -0300

    🚧 add clientutp.py and update old client

--- practicas/sockets/client.py
+++ practicas/sockets/client.py
@@ -5,7 +5,7 @@ import argparse
 if __name__ == '__main__':
     parser = argparse.ArgumentParser()
     parser.add_argument("-ip", "--host", help="Dirección IP o nombre del servidor al que conectarse", default="127.0.0.1")
-    parser.add_argument("-p", "--port", help="Número de puerto del servidor", default=65432)
+    parser.add_argument("-p", "--port", help="Número de puerto del servidor", default=9999)
 
     args = parser.parse_args()
     h = str(args.host)
